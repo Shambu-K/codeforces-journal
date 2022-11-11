ef38dfe3ecfd27b02c7b328eb6217e0b92540eae
Shambu-K <shambukavir958@gmail.com>
2022-11-11 10:35:47 +0530

    added files

---
+++ CodeTON Round 3 (Div. 1 + Div. 2, Rated, Prizes!)/B_Maximum_Substring.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()			{std::sort(this->begin(), this->end());}
+    void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+    auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+    auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+    friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+    friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define all(v)                   v.begin(), v.end()
+
+#define F  first
+#define S  second
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+
+void solve(){
+    int n;
+    string s;
+    cin >> n >> s;
+    long long lx = 0, ly= 0;
+    long long prod;
+    long long zero_count = 0, one_count = 0;
+    for(int i = 0; i < n; i++){
+        if(s[i] == '0') zero_count++;
+        else one_count++;
+    }
+    prod = zero_count*one_count;
+    long long ans = 0;
+    for(int i = 0; i < n; i++){
+        if(s[i] == '1'){
+            lx++;
+            ly = 0;
+        } 
+        else{
+            ly++;
+            lx = 0;
+        }
+        long long temp = max(lx*lx, ly*ly);
+        ans = max(ans, temp);
+
+    }
+    ans = max(ans, prod);
+    cout << ans;
+}
+
+int main(){
+    int T;
+    cin >> T;
+    FOR(t, T){
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}

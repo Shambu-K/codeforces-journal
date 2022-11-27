0a2971cab71089536beb179133e9cb3d0d957331
Shambu-K <shambukavir958@gmail.com>
2022-11-27 10:45:29 +0530

    not a good round

---
+++ Codeforces Global Round 24/B_Doremy_s_Perfect_Math_Class.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0)
+template<class T> struct V: vector<T>{using vector<T>::vector;
+    void sort()			{std::sort(this->begin(), this->end());}
+    void sort_dsc() 	{std::sort(this->begin(), this->end(), greater<T>());}
+    auto sum() 			{T sum = 0; for(auto& i: *this) sum += i; return sum;}
+    auto freqs() 		{map<T, int> freq; for(auto& i: *this) freq[i]++; return freq;}
+    friend ostream& operator<<(ostream& out, const V<T>& v) {for(auto& i: v) out << i << ' '; return out;}
+    friend istream& operator>>(istream& in, V<T>& v) {for(auto& i: v) in >> i; return in;}
+};
+#define pY {cout << "YES"; return;}
+#define pN {cout << "NO";  return;}
+
+#define FOR(i, n)                for(int i = 0; i < (int)n; ++i)
+#define all(v)                   v.begin(), v.end()
+
+#define F  first
+#define S  second
+constexpr char nl = '\n';
+constexpr int MOD = 1000000007;
+using ll  = long long int;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = V<int>;
+using vll = V<ll>;
+
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+
+void solve(){
+    int n;
+    cin >> n;
+    vi a(n);
+    cin >> a;
+    
+    int val = a[0];
+    for(auto i = 0; i < n; i++){
+        val = gcd(a[i], val);
+    }
+  
+    cout << a[n-1]/val;
+}
+int main(){
+    int T;
+    cin >> T;
+    FOR(t, T){
+        solve();
+        cout << nl;
+    }
+    
+    return 0;
+}
